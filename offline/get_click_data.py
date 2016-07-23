8588725bee77b209c142fa4a99e06920c03d98e7
leiyang-mids <ynglei@gmail.com>
2016-07-23 12:33:21 -0400

    array_agg it is!

--- offline/get_click_data.py
+++ offline/get_click_data.py
@@ -6,13 +6,12 @@ def get_click_data():
     '''
 
     page_interval = 1.5
-    conn = psycopg2.connect(database="aca_db", user="postgres", password="pass", host="localhost", port="5432")
+    conn = psycopg2.connect(database="w205", user="postgres", password="pass", host="localhost", port="5432")
     cur = conn.cursor()
 
-    sql_click = '(select c.session_id as sid, string_agg(c.click, \'|\') as c_t from (select session_id, plan_id || \',\' || created_clicks as click from Clicks) c) c2'
-    sql_rank = '(select r.session_id as sid, string_agg(r.rank, \'|\') as r_t from (select session_id, plan_id || \',\' || created_ranks || \',\' || plan_score as rank from Ranks) r) r2'
-    sql_exe = 'select q.session_id as sid, q.state as state, q.health as health, c2.c_t as click, r2.r_t as rank from \
-               Queries q join %s on q.session_id=c2.sid join %s on c2.sid=r2.sid' %(sql_click, sql_rank)
+    sql_click = '(select c.session_id as sid, array_agg(c.click) as c_t from (select session_id, plan_id || \',\' || created_clicks as click from Clicks) c group by c.session_id) c2'
+    sql_rank = '(select r.session_id as sid, array_agg(r.rank) as r_t from (select session_id, plan_id || \',\' || created_ranks || \',\' || plan_score as rank from Ranks) r group by r.session_id) r2'
+    sql_exe = 'select q.session_id as sid, q.state as state, q.health as health, c2.c_t as click, r2.r_t as rank from Queries q join %s on q.session_id=c2.sid join %s on c2.sid=r2.sid' %(sql_click, sql_rank)
     cur.execute(sql_exe)
     records = cur.fetchall()
     # define data type

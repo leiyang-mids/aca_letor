4810a298244509c08b2437febb665c501db73209
leiyang-mids <ynglei@gmail.com>
2016-07-23 20:24:07 -0400

    sparse distance matrix dimension.

--- offline/get_query_clusters.py
+++ offline/get_query_clusters.py
@@ -9,7 +9,7 @@ def get_query_clusters(points, k):
     # normalize input
     points = normalize(points.astype(np.float))
     # get similarity matrix (cosine distance)
-    dist = points.dot(points.T)
+    dist = points.dot(points.T).toarray()
     # initialize variables
     n_pt = points.shape[0]
     cluster_old, cluster_new = np.ones(n_pt), np.zeros(n_pt)
@@ -21,13 +21,13 @@ def get_query_clusters(points, k):
     while not np.array_equal(cluster_old, cluster_new):
         cluster_old = cluster_new
         # get cluster index for each point
-        cluster_new = np.argmax(points.dot(centroids.T), axis=1)
+        cluster_new = np.argmax(points.dot(centroids.T).toarray(), axis=1)
         # get new centroids, and within class mean distance/similarity
         centroids, in_dist = [], []
         for c in np.unique(cluster_new):
             pid = cluster_new==c
             # set new centroid as the one who has minimum total distance to rest of the points in the cluster
-            cid = np.argmax(np.sum(dist[np.ix_(pid, pid)], axis=1))
+            cid = 0 if sum(pid)==1 else np.argmax(np.sum(dist[np.ix_(pid, pid)], axis=1))
             centroids.append(points[pid][cid])
             in_dist.append(1 if sum(pid)==1 else np.mean(dist[np.ix_(pid,pid)][np.triu_indices(sum(pid),k=1)]))
         centroids = np.array(centroids)

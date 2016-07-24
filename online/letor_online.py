c3cfc6459dc6beecb2a60e2dbd43d94c9f6f052f
leiyang-mids <ynglei@gmail.com>
2016-07-23 23:47:37 -0400

    refactor letor_online

--- online/letor_online.py
+++ online/letor_online.py
@@ -1,5 +1,5 @@
 
-from sklearn.feature_extraction.text import *
+# from sklearn.feature_extraction.text import *
 from sklearn.preprocessing import normalize
 import numpy as np
 
@@ -7,34 +7,22 @@ class letor_online:
     ''' runtime component for letor, give plan ranking for query
     '''
     characterizer = None
-    learnt_query = None
-    plan_ranks = None
-    encode_query = None
-    normalize_query = None
+    centroids = None
     similarity_limit = 0.8
-    n_query = 0
 
-    def __init__(self, learnt_query, plan_ranks, similarity = 0.8):
+    def __init__(self, query_characterizer, cluster_centroids, similarity = 0.8):
         '''
-        learnt_query [nx1]: a list of n strings, each one is a query
-        plan_ranks [m:1xn]: a dictionary with m key-value pairs;
-                            key is plan_id, value is a [1xn] list of plan weight for each query
+        query_characterizer : a CountVectorizer object fitted with learned queries
+        cluster_centroids   : center points of query clusters
         '''
-        # TODO: data uniformity check
-
-        self.learnt_query = learnt_query
-        self.n_query = len(learnt_query)
-        self.plan_ranks = plan_ranks
         # initialize encoder
-        self.characterizer = CountVectorizer()
-        self.encode_query = self.characterizer.fit_transform(learnt_query)
-        self.normalize_query = normalize(self.encode_query.astype(np.float))
+        self.characterizer = query_characterizer
+        self.centroids = cluster_centroids
         self.similarity_limit = similarity
 
-    def get_rank(self, query):
+    def get_rank_weight(self, query):
         '''
-        get plan rank for a query, if no similar query if found, None is returned
-        query [str]: a string for query
+        get a vector to dot product with indexed ranking in ES from training
         '''
         if type(query) is not str:
             print 'Input parameter query should be a string.'
@@ -47,19 +35,15 @@ class letor_online:
             return None
 
         # set all values to 1 of encoded query (don't care duplicate terms in query)
-        for i in range(vQuery.data.size):
-            vQuery.data[i] = 1
+        vQuery.data = np.ones(vQuery.data.size)
 
         # find the most similar query, by cosine distance
-        similarity = normalize(vQuery.astype(np.float)).dot(self.normalize_query.T)
+        similarity = normalize(vQuery.astype(np.float)).dot(self.centroids.T)
         candidates = similarity > self.similarity_limit
-        if sum(candidates.data) == 0:
-            # if no one above limit, return the synthesized ranking from the closest queries
-            candidates = similarity == max(similarity.data)
+        # if no one above limit, return the synthesized ranking from the closest queries
+        if candidates.sum() == 0:
+            candidates = similarity == similarity.max()
 
-        # synthesize rank to return
-        index = [i for i in range(self.n_query) if candidates.getcol(i)]
-        print 'return %d closest match query with index: %s' %(len(index), str(index))
-        sum_sim = sum(similarity.getcol(i).data[0] for i in index)
-        weights = [similarity.getcol(i).data[0]/sum_sim for i in index]
-        return {pid:sum(rank[i]*w for i,w in zip(index,weights)) for pid,rank in self.plan_ranks.items()}
+        # weight vector for dot product
+        sum_sim = similarity[candidates].sum()
+        return [s/sum_sim if c else 0 for s,c in zip(similarity.toarray()[0],candidates.toarray()[0])]

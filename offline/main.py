269fcf5ab4bcf7ec9e6e9a33f5d1ff976418c4b7
leiyang-mids <ynglei@gmail.com>
2016-07-25 21:42:16 -0400

    generate test click data

--- offline/main.py
+++ offline/main.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
 from get_click_data import *
 from train_one_state import *
-from logger import *
-from s3_helpers import *
 import traceback, time
 import numpy as np
 
@@ -11,35 +9,36 @@ from simulate_clicks import *
 def main():
 	'''
 	'''
-	next_run, hour, minute = datetime.now(), 1, 10
+	next_run, hour, minute = datetime.now(), 5, 10
 	s3clnt, log = s3_helper(), logger('training')
+	test = False
 
 	while True:
-		# cyclic execution
+
 		if datetime.now() < next_run:
 			time.sleep((next_run-datetime.now()).total_seconds())
 			continue
 		# get click-through data
 		try:
-			log.trace('get query and click data')
-			click_data = get_click_data(log)
+			print 'get query and click data'
+			click_data = get_click_data() if not test else simulate_clicks()
 		except Exception as ex:
 			traceback.print_exc(file=log.log_handler())
-			log.trace('error getting click data, retry in %d minutes.' %minute)
+			print 'error getting click data, retry in %d minutes.' %minute
 			next_run = datetime.now() + timedelta(minutes=minute)
 			continue
 		# train for each state
 		for state in np.unique(click_data['state']):
 			try:
-				train_one_state(click_data, state, log)
+				train_one_state(click_data, state)
 			except KeyboardInterrupt:
 				sys.exit('User termination')
 			except Exception as ex:
 				traceback.print_exc(file=log.log_handler())
-				log.trace('training has encountered an error for state %s' %state)
+				print 'training has encountered an error for state %s' %state
 		# training completed, get next run time
 		next_run = datetime.now() + timedelta(hours=hour)
-		log.trace('training has completed, next run time is %s' %str(next_run))
+		print 'training has completed, next run time is %s' %str(next_run)
 		# upload log file to S3
 		s3clnt.upload2(log.log_name(), 'log/'+log.log_name())
 
--- offline/simulate_clicks.py
+++ offline/simulate_clicks.py
@@ -1,17 +1,29 @@
 import numpy as np
 import pickle
 
-def simulate_clicks(n_query = 8):
-    ''''''
+def simulate_clicks(n_query = 8, n_click_max = 8):
+    '''
+    '''
 
-    savedData = 'feature/UT_74_19243.pickle'
-    with open(savedData) as f:
-        feature, plan = pickle.load(f)
-    plan=np.array(plan)
-    n_plan, n_fea = feature.shape
-    sim_click = np.array([[plan[np.random.permutation(n_plan)],
-                       plan[np.random.permutation(n_plan)[0:np.random.randint(low=1, high=10)]]]
-                      for i in range(n_query)])
-    q_cluster = np.random.random_integers(0,2,n_query)
-    print 'simulated clicks with %d queries' %n_query
-    return plan, q_cluster, sim_click
+    with open('plan_ids.pickle') as f:
+        all_plan = pickle.load(f)
+
+    # sim health
+    sim_health = np.array(['Alzheimer', 'Glaucoma', 'Hypertension', 'Obesophobia', 'diabetes'])
+
+    # get states
+    sim_data = []
+    state_ids = np.unique([i[5:7] for i in all_plan[0]])
+    for state in state_ids:
+        state_plan = np.array([i for i in all_plan[0] if state in i])
+        n_plan = len(state_plan)
+#         print 'state %s has %d plans' %(state, n_plan)
+        # simulate query
+        for i in range(n_query):
+            health = ' '.join(sim_health[np.random.permutation(sim_health.size)[0:i+1]])
+            sim_rank = state_plan[np.random.permutation(n_plan)]
+            sim_click = state_plan[np.random.permutation(n_plan)][0:np.random.randint(low=1, high=min(n_plan,n_click_max))]
+            sim_data.append((state, health, sim_rank, sim_click))
+
+    rtn_type = [('state','S2'), ('query','S512'), ('ranks',list), ('clicks',list)]
+    return np.array(sim_data, dtype=rtn_type)

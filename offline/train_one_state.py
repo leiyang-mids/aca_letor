6e15afc1a340842c3058c8606b196568e9216139
leiyang-mids <ynglei@gmail.com>
2016-08-06 09:31:47 -0400

    clean up

--- offline/train_one_state.py
+++ offline/train_one_state.py
@@ -20,14 +20,11 @@ def train_one_state(click_data, state, log, s3_fea):
     # exclude missing plan IDs in ES
     with open('missing.pickle') as f:
         missing = pickle.load(f)
-    picker = np.array([p not in missing for p in plans])
-    plans = list(np.array(plans)[picker])
-    for i in range(len(letor_rank)):
-        letor_rank[i] = letor_rank[i][picker]
+    picker = np.array([p not in missing for p in plans])    
     # upload the stuff to S3
     save_training = 'training/%s_%d.pickle' %(state, len(letor_rank))
     with open(save_training, 'w') as f:
-        pickle.dump([plans, letor_rank], f)
+        pickle.dump([list(np.array(plans)[picker]), letor_rank[:, picker]], f)
     s3clnt.delete_by_state('training/%s' %(state))
     s3clnt.upload(save_training)
     save_online = 'online/%s_runtime.pickle' %(state)
